a2968af2b4705601249a074bb9c446339fea715b
baal098764 <baalb176@gmail.com>
2024-11-21 23:10:18 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ def main_page():
 
     header_1 = st.subheader("Extract Post Media", divider = "rainbow")
 
-    url = st.text_input("VSCO Post URL:",
-                        placeholder = "https://vsco.co/emilieristevski/media/561f648001146426743090fa",
-                        help = "URL of the VSCO post from which media should be extracted.")
+    url_input = st.text_input("VSCO Post URLs (comma-separated):",
+                             placeholder = "https://vsco.co/scarlettmccarthyy/media/673a69d397d0f92b0954ce43, https://vsco.co/user2/media/12345",
+                             help = "Comma-separated list of VSCO post URLs from which media should be extracted.")
 
     l1, l2, center_button, r1, r2 = st.columns(5)
 
@@ -39,9 +39,13 @@ def main_page():
         download = st.button("Download!", type = "primary", use_container_width = True)
 
     if download:
-        media_urls = d(url, True, False)
+        # Split the input string by commas and process each URL
+        urls = [url.strip() for url in url_input.split(',')]  # Split by comma and remove any extra spaces
+        media_urls = []
+        for url in urls:
+            media_urls.extend(d(url, True, False))  # Fetch media URLs for each input URL
         header_2 = st.subheader("Extracted Post Media", divider = "rainbow")
-        display_media_urls = st.markdown("\n".join([f"- Found media url: [{media_url}]({media_url})" for media_url in media_urls]))
+        display_media_urls = st.markdown("\n".join([f"- Found media URL: [{media_url}]({media_url})" for media_url in media_urls]))
 
 # side bar and main page loader
 def main():
@@ -67,14 +71,10 @@ def main():
     logo = st.sidebar.image(".streamlit/ico/download-icon.png",
                             caption = "VSCO Downloader is a simple script to extract raw image and video paths from VSCO posts.")
 
-    #doc = st.sidebar.markdown(about_str)
-
     div_2 = st.sidebar.subheader("", divider = "rainbow")
 
     info_str = ""
-    info_str += "- **Contact:**  \n  [micha.birklbauer@gmail.com](mailto:micha.birklbauer@gmail.com)\n"
-    info_str += "- **License:**  \n  [MIT License](https://github.com/michabirklbauer/vsco_downloader/blob/master/LICENSE.md)\n"
-    info_str += "- **Project Page:**  \n  [GitHub](https://github.com/michabirklbauer/vsco_downloader/)"
+    info_str += "- **Contact:**  \n  [TTKgroups](https://t.me/TTKgroups)\n"
     info = st.sidebar.markdown(info_str)
 
     main_page()
